1a0af7a8f658e9fb380347787e9b49797dbdf910
nicol29 <nicolascondrea@gmail.com>
2022-11-17 19:30:57 +0000

    Integrate random colour functionality

--- app.js
+++ app.js
@@ -1,9 +1,12 @@
 let gridSize = 16;
 let squaresAmount = gridSize * gridSize;
+let isRainbowModeOn = false;
+let buttonCounter = 1;
 
 const gridContainer = document.querySelector(".grid-container");
 const submitButton = document.querySelector("button");
-let textBoxInput = document.querySelector("input");
+const textBoxInput = document.querySelector("input");
+const rainbowButton = document.querySelector(".multi-colour");
 
 
 function createGrid(gridSize){
@@ -19,15 +22,34 @@ function createGrid(gridSize){
     let divsOnScreen = document.querySelectorAll(".grid-container div")
 
     divsOnScreen.forEach(grid => grid.addEventListener("mouseover", () => {
-        grid.style.backgroundColor = "black";
+        if(isRainbowModeOn){
+            grid.style.backgroundColor = genRandColour();
+        } else{
+            grid.style.backgroundColor = "black";
+        }
     }));
 }
 
 
 submitButton.onclick = () => {
     if(textBoxInput.value >= 16 && textBoxInput.value <= 100){
-        let newGridSize = textBoxInput.value;
-        gridSize = newGridSize;
+        gridSize = textBoxInput.value;
+        textBoxInput.value = "";
+        clearGrid();
+    }
+}
+
+
+rainbowButton.onclick = () => {
+    buttonCounter ++;
+
+    if(buttonCounter % 2 == 0){
+        rainbowButton.classList.add("rainbow")
+        isRainbowModeOn = true;
+        clearGrid();
+    } else{
+        rainbowButton.classList.remove("rainbow")
+        isRainbowModeOn = false;
         clearGrid();
     }
 }
@@ -41,6 +63,12 @@ function clearGrid(){
     }
 
     createGrid(gridSize);
+}
+
+
+function genRandColour(){
+    let randomColour = (Math.random() * 0xfffff * 1000000).toString(16);
+    return "#" + randomColour.slice(0, 6);
 }
 
 
